02c61dfccf441b9bc2b110b2ab26fcde2b0623d3
bear <bear@a123065e-f578-804e-ac06-b1832d43b128>
2006-10-05 12:15:11 +0000

    update
    git-svn-id: http://svn.nate.deepcreek.org.au/svn/DigitalWatch/trunk@181 a123065e-f578-804e-ac06-b1832d43b128

--- bin/Filters/DWRegister.bat
+++ bin/Filters/DWRegister.bat
@@ -1,6 +1,14 @@
-regsvr32 TSFileSource.ax /s
-regsvr32 DSNet.ax /s
-rem regsvr32 MpaDecFilter.ax /s
-rem regsvr32 Mpeg2DecFilter.ax /s
-rem regsvr32 MpgMux.ax /s
+cd Filters
+
+%WINDIR%\system32\regsvr32.exe TSFileSource.ax /S /u
+rem copy TSFileSource.ax %WINDIR%\system32
+%WINDIR%\system32\regsvr32.exe TSFileSource.ax /s
+
+%WINDIR%\system32\regsvr32.exe DSNet.ax /S /u
+copy DSNet.ax %WINDIR%\system32
+%WINDIR%\system32\regsvr32.exe DSNet.ax /s
+
+rem regsvr32 MpaDecFilter.ax
+rem regsvr32 Mpeg2DecFilter.ax
+rem regsvr32 MpgMux.ax
 rem Pause
